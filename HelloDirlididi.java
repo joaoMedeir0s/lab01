5cdbbc5b89b1dd59ded054217fd9e08b6ada6a2c
João Pedro <joao.pedro.araujo.de.medeiros@ccc.ufcg.edu.br>
2024-11-21 16:23:48 -0300

    Add files via upload

---
+++ HelloDirlididi.java
@@ -0,0 +1,11 @@
+/**
+ * Laboratório de Programação 2 - Lab 1
+ *
+ * @João Pedro Araújo de Medeiros - 124110741
+ */
+
+public class HelloDirlididi{
+       public static void main(String[] args){
+		System.out.println("Hello Dirlididi!");
+       }
+}     

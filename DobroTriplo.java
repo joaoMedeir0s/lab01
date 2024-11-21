aee586ba7eec1c8570181a64e4065300abb2da99
João Pedro <joao.pedro.araujo.de.medeiros@ccc.ufcg.edu.br>
2024-11-21 16:24:32 -0300

    Add files via upload

---
+++ DobroTriplo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * Laboratório de Programação 2 - Lab 1
+ * 
+ * @João Pedro Araújo de Medeiros - 124110741
+ */
+
+import java.util.Scanner;
+
+public class DobroTriplo{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		int num = input.nextInt();
+		int dobro = num * 2;
+		int triplo = num * 3;
+		System.out.println("dobro: " + dobro + ", triplo: " + triplo);
+	}
+}
+		
+
